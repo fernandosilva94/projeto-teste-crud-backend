6977cb7acf5c557de6ad173378b982a19652ab58
Fernando Silva <fernandoadmti@hotmail.com>
2024-05-08 22:41:37 -0300

    finalizando endpoints

--- src/main/java/com/ship/projetoteste/cadastro/controller/UserController.java
+++ src/main/java/com/ship/projetoteste/cadastro/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ship.projetoteste.cadastro.dto.UserDTO;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import com.ship.projetoteste.cadastro.model.User;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,18 +22,32 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
     @PostMapping(value = "/add")
     @Transactional
     public ResponseEntity<User> addUser(@Valid @RequestBody UserDTO userDTO) {
         User newUser = userService.addUser(userDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
-
     @GetMapping(value = "/get-all")
     public ResponseEntity <List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUse(@PathVariable Long id, @Valid @RequestBody UserDTO userDetails) {
+        User updateUser = userService.updateUser(id, userDetails);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
