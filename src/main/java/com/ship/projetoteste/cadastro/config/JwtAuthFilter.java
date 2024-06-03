63c671afd3f6e18f120ee1aad25509e24d6e714c
Fernando Silva <fernandoadmti@hotmail.com>
2024-06-03 17:06:53 -0300

    ajustando configuracoes do spring security e jwt

--- src/main/java/com/ship/projetoteste/cadastro/config/JwtAuthFilter.java
+++ src/main/java/com/ship/projetoteste/cadastro/config/JwtAuthFilter.java
@@ -4,14 +4,25 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-
+@Component
+@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final UserDetailsService userDetailsService;
+    private final JwtUtils jwtUtils;
+
     @Override
     protected void doFilterInternal (HttpServletRequest request,
                                     HttpServletResponse response,
@@ -25,9 +36,18 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             return;
         }
         jwtToken = authHeader.substring(7);
-        userEmail = "something";
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        userEmail = jwtUtils.extractUsername(jwtToken);
 
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+
+            if (jwtUtils.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/ship/projetoteste/cadastro/config/SecurityConfig.java
+++ src/main/java/com/ship/projetoteste/cadastro/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ship.projetoteste.cadastro.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig  {
+
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         HttpBasicConfigurer<HttpSecurity> httpSecurityHttpBasicConfigurer = http
@@ -18,6 +22,7 @@ public class SecurityConfig  {
                 .anyRequest()
                 .authenticated()
                 .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic();
         return http.build();
     }
